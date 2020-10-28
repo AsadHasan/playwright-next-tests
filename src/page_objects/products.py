6e7da775b8716096c69a26019c3fdee9408f1abe
AsadHasan <asadhasan180@hotmail.com>
2020-10-28 14:54:03 +0000

    extended test to cover more ground

--- src/page_objects/products.py
+++ src/page_objects/products.py
@@ -1,6 +1,7 @@
 """Products Page-object"""
 from __future__ import annotations
-from playwright.sync_api import Page
+from typing import List
+from playwright.sync_api import ElementHandle, Page
 
 
 class Products:
@@ -34,6 +35,29 @@ class Products:
         """Get products page's heading
 
         Returns:
-            str: [Products page's heading
+            str: Products page's heading
         """
         return self.page.innerText("#ResultHeader > div.SearchedFor > h1")
+
+    def get_number_of_results(self) -> int:
+        """Get total number of results
+
+        Returns:
+            int: Total number of results
+        """
+        number_of_results_text = self.page.innerText("#total-results-count")
+        return [
+            int(character)
+            for character in number_of_results_text.split()
+            if character.isdigit()
+        ][0]
+
+    def all_results_thumbnail_images_have_newin_banner(self) -> bool:
+        """Do all result images have "New In" banner?
+
+        Returns:
+            bool: Whether all result images have "New In" banner
+        """
+        selector: str = ".Page .Images"
+        images: List[ElementHandle] = self.page.querySelectorAll(selector)
+        return all(img.querySelector(".NewIn") is not None for img in images)
--- src/test_next_website.py
+++ src/test_next_website.py
@@ -2,24 +2,35 @@
 from playwright.sync_api import Page
 
 from page_objects.homepage import Homepage
+from page_objects.products import Products
 
 
 def test_search_and_filter_mens_shoes(page: Page) -> None:
     """Failure of this test implies that Men's shoes cannot
-    be searched and then filtered by "New" and "In stock"
-    filters
+    be searched and then correctly filtered by "New" and
+    "In stock" filters.
 
     Args:
         page (Page): Browser page/tab
     """
-    heading: str = (
-        Homepage(page)
-        .open()
-        .select_category("MEN")
-        .select_sub_category("Shoes")
-        .filter_by_new_in()
-        .filter_by_in_stock()
-        .get_products_heading()
+    # Search for Men's shoes
+    homepage: Homepage = Homepage(page)
+    products_page: Products = (
+        homepage.open().select_category("MEN").select_sub_category("Shoes")
     )
+    number_of_unfiltered_results: int = products_page.get_number_of_results()
+
+    # Filter by "New In" and "In Stock"
+    products_page.filter_by_new_in().filter_by_in_stock()
+    number_filtered_results = products_page.get_number_of_results()
+    heading: str = products_page.get_products_heading()
+
+    # Check heading shows filtering
     assert "New In" in heading
     assert "In Stock" in heading
+
+    # Check results count correctly changed after filtering
+    # and that all result images show "New In" filter banner
+    assert number_filtered_results <= number_of_unfiltered_results
+    if number_filtered_results > 0:
+        assert products_page.all_results_thumbnail_images_have_newin_banner()
